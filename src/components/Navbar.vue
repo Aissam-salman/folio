29a8831e1f6c9ed11614bbf41956dadfa447f6a3
”Aissam-salman” <nasawi.lmj@gmail.com>
2025-10-08 15:47:05 +0200

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { RouterLink, useRoute } from 'vue-router'
+const route = useRoute()
+const isActive = (path: string) => {
+  return route.path == path
+}
+</script>
+<template>
+  <div class="flex justify-center p-5 z-10">
+    <header class="w-fit border-1 border-gray-600/30 rounded-lg backdrop-blur-sm bg-white/5">
+      <nav
+        :class="
+          'py-2 px-10 flex items-center flex-row gap-10 ' +
+          (isActive('/') ? 'text-purple-50' : 'text-slate-900')
+        "
+      >
+        <RouterLink to="/" :class="isActive('/') ? 'font-bold' : 'font-normal'"> Home </RouterLink>
+        <RouterLink to="/work" :class="isActive('/work') ? 'font-bold' : 'font-normal'"
+          >Work</RouterLink
+        >
+        <RouterLink to="/about" :class="isActive('/about') ? 'font-bold' : 'font-normal'"
+          >About</RouterLink
+        >
+      </nav>
+    </header>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Dither from '@/components/Dither.vue'
+</script>
+<template>
+  <Dither
+    :wave-speed="0.025"
+    :wave-frequency="5"
+    :wave-amplitude="0.1"
+    :wave-color="[0.9, 0.2, 0.9]"
+    :color-num="12"
+    :pixel-size="2"
+    :disable-animation="false"
+    :enable-mouse-interaction="true"
+    :mouse-radius="1"
+  />
+  <main>HOME</main>
+</template>
