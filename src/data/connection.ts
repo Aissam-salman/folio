825517cc629b78f01b0f19154b6203c825666972
”Aissam-salman” <nasawi.lmj@gmail.com>
2025-10-10 19:14:50 +0200

    Refactor environment variables and add new components for GitHub and LinkedIn icons, along with project display functionality.

--- src/data/connection.ts
+++ src/data/connection.ts
@@ -1,6 +1,6 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://cxsjanaqlneuyhqubtmt.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = import.meta.env.SUPABASE_URL
+const supabaseKey = import.meta.env.SUPABASE_KEY
+export const supabase = createClient(supabaseUrl, supabaseKey)
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,33 @@
 <script setup lang="ts">
 import Enter from '@/components/Animations/Enter.vue'
 import Dither from '@/components/Dither.vue'
+import Github from '@/components/Icon/Github.vue'
+import Linkedin from '@/components/Icon/Linkedin.vue'
 </script>
 <template>
-  <Dither
-    :wave-speed="0.025"
-    :wave-frequency="5"
-    :wave-amplitude="0.1"
-    :wave-color="[0.9, 0.2, 0.9]"
-    :color-num="12"
-    :pixel-size="2"
-    :disable-animation="false"
-    :enable-mouse-interaction="true"
-    :mouse-radius="1"
-    class="-z-10"
-  />
+  <Dither :wave-speed="0.025" :wave-frequency="5" :wave-amplitude="0.1" :wave-color="[0.9, 0.2, 0.9]" :color-num="12"
+    :pixel-size="2" :disable-animation="false" :enable-mouse-interaction="true" :mouse-radius="1" class="-z-10" />
   <main class="text-white flex justify-center">
     <Enter>
-      <h1 class="font-extrabold text-3xl">Hey, I'm Aissam-Salman, Full stack-developer</h1>
+      <div class="flex flex-col gap-3">
+        <h1 class="font-extrabold text-3xl">Aissam-Salman, Full-stack developer</h1>
+        <p class="flex flex-col">
+          <span>Né en 1994, j'ai grandi avec le web, passé beaucoup de temps à l'explorer.</span>
+          <span>J'aimerais maintenant participer à son évolution, apporter de nouveaux usages,</span>
+          <span>des designs qui resteront dans les mémoires de chaque utilisateur.</span>
+        </p>
+        <div class="flex gap-6 justify-center mt-32">
+          <a class="cursor-pointer" href="https://www.linkedin.com/in/aissam-salman-webdev/" target="_blank">
+            <Linkedin />
+          </a>
+          <a class="cursor-pointer" href="https://github.com/Aissam-salman" target="_blank">
+            <Github />
+          </a>
+        </div>
+        <section>
+          <Projects />
+        </section>
+      </div>
     </Enter>
   </main>
 </template>
