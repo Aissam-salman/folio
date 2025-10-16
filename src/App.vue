2716e43fb7880d1976158e34611e26d5e7a69b94
Aissam-salman <nasawi.lmj@gmail.com>
2025-10-16 19:01:46 +0200

    Add SpeedInsights component to App.vue for performance insights

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 </script>
 
 <template>
   <div class="bg-[#171717] text-white min-h-screen">
+    <SpeedInsights />
     <RouterView />
   </div>
 </template>
